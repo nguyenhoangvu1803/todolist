42cb9989bd4f952a0776305f22f7ac39568020c5
Vu Hoang <nguyenhoangvu1803@gmail.com>
2019-11-06 10:04:46 +0700

    change css js

--- views/layouts/application.php
+++ views/layouts/application.php
@@ -5,12 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Demo PHP MVC</title>
-	<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-	<script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.13.1/jquery.validate.min.js"></script>
-	<script src="assets/js/main.js"></script>
+	<link rel="stylesheet" href="assets/css/jquery-ui.css">
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<script src="assets/js/jquery-1.12.4.js"></script>
+	<script src="assets/js/jquery-1.12.1-ui.js"></script>
+	<script src="assets/js/jquery.validate.min.js"></script>
+	<script type="text/javascript" src="assets/js/main.js"></script>
   </head>
   <body>
 	<div class="container">
